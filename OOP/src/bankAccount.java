1a4d6ac095bb64e24922ab6f9286e1524c635b0b
fmbic <fedkamedved@gmail.com>
2025-11-07 20:03:48 +0300

    OOP bank account

---
+++ OOP/src/bankAccount.java
@@ -0,0 +1,24 @@
+public class bankAccount
+{
+    private int accountNumber;
+    private double balance;
+
+    public  double deposit(double amount)
+    {
+        return 0;
+    }
+
+    public double withdraw (double amount)
+    {
+        return 0;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+
+
+
+}
