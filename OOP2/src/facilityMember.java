97a25f9c7bb1e40d099821cffa630411bb30743e
fmbic <fedkamedved@gmail.com>
2025-11-12 20:13:37 +0300

    polymorphism for facility member

--- OOP2/src/facilityMember.java
+++ OOP2/src/facilityMember.java
@@ -1,10 +1,15 @@
-public class facilityMember extends libraryMember
-{
-    public void checkBooks()
-    {
-        if (borrowedBooks > 10)
-        {
-            System.out.println("Your limit is reached, you borrowed "+ borrowedBooks+" books");
+public class facilityMember extends libraryMember {
+    public void checkBooks() {
+        if (borrowedBooks > 10) {
+            System.out.println("Your limit is reached, you borrowed " + borrowedBooks + " books");
+        }
+    }
+
+    public void calculateFine(int daysOverdue) {
+        double fineAmount = 0;
+
+        if (daysOverdue > 14) {
+            fineAmount = (daysOverdue - 14) * 0.5;
         }
     }
 }
