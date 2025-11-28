5aff209307227980afc0403e1665368d647b7cd8
fmbic <fedkamedved@gmail.com>
2025-11-29 00:27:27 +0300

    come back

---
+++ src/iterPractice.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class iterPractice
+{
+    public class book
+    {
+        ArrayList<String> books = new ArrayList<String>();
+
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
