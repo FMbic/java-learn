35c5661cbc2c639416d2a32a1f5f658bb98a08b3
fmbic <fedkamedved@gmail.com>
2025-11-29 12:29:03 +0300

    adding books and printing them

--- src/iterPractice.java
+++ src/iterPractice.java
@@ -3,17 +3,22 @@ import java.util.List;
 import java.util.Collections;
 import java.util.Iterator;
 
-
- public void  book()
-    {
-        ArrayList<String> books = new ArrayList<String>();
-        Collections.addAll(books, "The Hobbit", "1984", "Brave New World");
-
-
-    }
-
     public static void main(String[] args)
     {
+        ArrayList<String> books = new ArrayList<String>();
 
+        ///Collections.addAll(books, "The Hobbit", "1984", "Brave New World");
+        books.add("The Hobbit");
+        books.add("1984");
+        books.add("Brave new World");
+
+        Iterator<String> it = books.iterator();
+
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+
+        //System.out.println(books);
     }
 
