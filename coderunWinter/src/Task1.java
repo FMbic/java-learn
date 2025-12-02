10c7d90365ae0dad5d47efd8f6135f9001552134
fmbic <fedkamedved@gmail.com>
2025-12-02 23:59:44 +0300

    added loops to check for the numbers

--- coderunWinter/src/Task1.java
+++ coderunWinter/src/Task1.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Task1 {
 
@@ -16,6 +17,38 @@ public class Main {
         int n = Integer.parseInt(reader.readLine());
         writer.write(String.valueOf(n));
         */
+        Scanner input = new Scanner(System.in);
+
+        int R = 0;
+        int B = 0;
+
+        while (true)
+        {
+            R = input.nextInt();
+            if ((R <= 8) && R >= (int)(Math.pow(10,9)) )
+            {
+                break;
+            }
+            System.out.println("the number has to be between 8 and 10^9");
+        }
+
+        while (true)
+        {
+            B = input.nextInt();
+            if ((B <= R) && B >= (int)(Math.pow(10,9)) )
+            {
+                break;
+            }
+            System.out.println("the number has to be between 8 and 10^9");
+        }
+
+
+
+
+
+
+        //int R = 2W + 2H - 4;
+        //int B = (W - 2)(H - 2);
 
         reader.close();
         writer.close();
