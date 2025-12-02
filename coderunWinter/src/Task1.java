07dfef1b330d74da0c53875ca3e0911aede639c7
fmbic <fedkamedved@gmail.com>
2025-12-03 00:34:40 +0300

    it works

--- coderunWinter/src/Task1.java
+++ coderunWinter/src/Task1.java
@@ -5,9 +5,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
-public class Task1 {
 
-public class Main {
+
+public class Task1 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -17,11 +17,7 @@ public class Main {
         int n = Integer.parseInt(reader.readLine());
         writer.write(String.valueOf(n));
         */
-        Scanner input = new Scanner(System.in);
-
-        int R = 0;
-        int B = 0;
-
+        /*
         while (true)
         {
             R = input.nextInt();
@@ -43,15 +39,33 @@ public class Main {
         }
 
 
+         */
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введи число от 8 до 10^9");
+        System.out.println("Введи число от 1 до 10^9");
 
 
+         */
+        String [] strs = reader.readLine().split(" ");
 
 
+        int B = Integer.parseInt(strs[1]);
+        int R = Integer.parseInt(strs[0]);
+
+
+        int S = (R + 4)/2;
+        int P = B + 2 * S - 4;
+        int D = S * S - 4 * P;
+        int sqrt = (int)Math.sqrt(D);
+        int W = (S + sqrt)/2;
+        int H = (S - sqrt)/2;
+
+        System.out.println(W+ " " + " "+H);
         //int R = 2W + 2H - 4;
         //int B = (W - 2)(H - 2);
 
         reader.close();
         writer.close();
     }
-}
 }
